e943025c555ca83059d07cee730e2b626fb213a6
Jay Parikh <jbparikh27@gmail.com>
2025-02-25 14:54:46 +0530

    Add files via upload

---
+++ GrpcServer/Models/ProductItem.cs
@@ -0,0 +1,11 @@
+﻿namespace GrpcServer.Models
+{
+    public class ProductItem
+    {
+        public int ProductId { get; set; }
+        public required string ProductName { get; set; }
+        public required string ProductDescription { get; set; }
+        public double ProductPrice { get; set; }
+
+    }
+}
